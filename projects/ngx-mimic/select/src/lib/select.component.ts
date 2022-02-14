2149fd6f65324718eb7d66dd0e6f0e5aa72304a2
sleep-written <sleep.written0000@gmail.com>
2022-02-14 12:50:04 -0300

    Create projects

---
+++ projects/ngx-mimic/select/src/lib/select.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-select',
+  template: `
+    <p>
+      select works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-mimic/select/src/lib/select.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SelectComponent } from './select.component';
+
+
+
+@NgModule({
+  declarations: [
+    SelectComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SelectComponent
+  ]
+})
+export class SelectModule { }
